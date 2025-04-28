be6277c4928c02154bc8edbca6ff8350822ff953
Perny <83672513+pernydev@users.noreply.github.com>
2025-04-28 15:13:19 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-setInterval(() => {
-  if (Math.random() < 0.5) {
-    console.error("Hello, World! (stderr)");
-  } else {
-    console.log("Hello, World! (stdout)");
-  }
-}, 1000);
+const socket = new WebSocket("ws://localhost:4000");
+
+socket.addEventListener("open", (event) => {
+  console.log("opened:", event);
+});
+socket.addEventListener("message", (event) => {
+  console.log("recv:", event.data);
+});
+socket.addEventListener("close", (event) => {
+  console.log("closed:", event);
+});
+socket.addEventListener("error", (event) => {
+  console.error("error:", event);
+});
