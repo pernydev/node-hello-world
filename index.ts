ba7622a61c72b92aeb3545cf2950007b1bb5716d
Perny <83672513+pernydev@users.noreply.github.com>
2025-05-06 17:44:10 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-client.once("ready", () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content.toLowerCase() === "ping") {
-    message.reply("pong");
+client.on('messageCreate', (message) => {
+  if (message.content.toLowerCase() === 'ping') {
+    message.reply('pong');
   }
 });
 
